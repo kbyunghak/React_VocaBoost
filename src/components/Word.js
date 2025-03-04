1dd39017d4c8902a0446c3a4a74e0df23a1f3bc6
AndrewKim <andrew.kim@ballard.com>
2025-03-03 16:10:56 -0800

    Add API feature for put and delete

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
-export default function Word({ word}) {
+export default function Word({ word : w }) {
+    const [word, setWord] = useState(w);
     const [isShow, setIsShow] = useState(false);
     const [isDone, setIsDone] = useState(word.isDone);
 
@@ -8,14 +9,48 @@ export default function Word({ word}) {
         setIsShow(!isShow);
     }
 
-    function toggleIsDone() {
-        setIsDone(!isDone);
+    function toggleDone() {
+        //setIsDone(!isDone);
+        fetch(`http://localhost:3001/words/${word.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },            
+            body: JSON.stringify({ 
+                ...word, 
+                isDone: !isDone 
+            }),
+        })
+        .then(res => {
+            if (res.ok) {
+                setIsDone(!isDone); 
+            }
+        }); 
     }
+
+    function del() {
+        if (window.confirm('Are you sure?')) {
+            fetch(`http://localhost:3001/words/${word.id}`, {
+                method: 'DELETE',
+            })
+            .then(res => {
+                if (res.ok) {
+                    setWord({ id: 0 });
+                }
+            });
+        } else {
+            alert('Cancelled');
+        }   
+    }
+
+    if (word.id === 0) {
+        return null;
+    }   
 
     return (
         <tr className={ isDone ? "off" : "" }>
             <td>
-                <input type="checkbox" checked={isDone} onChange={toggleIsDone}/>
+                <input type="checkbox" checked={isDone} onChange={toggleDone}/>
             </td>
             <td>{word.day}</td>
             <td>{word.word}</td>
@@ -26,7 +61,7 @@ export default function Word({ word}) {
                 </button>
             </td>
             <td>                                                    
-                <button className="btn_del">Delete</button>
+                <button className="btn_del" onClick={ del }>Delete</button>
             </td>
         </tr>
     );
